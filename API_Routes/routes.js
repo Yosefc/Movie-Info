6570e5b0d448d0650a472ade9e1641940176c9ba
Yosefc <yosef.conn@gmail.com>
2019-08-15 11:19:38 +0300

    Changed API URLs

--- API_Routes/routes.js
+++ API_Routes/routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const API_KEY = process.env.OMDB_API_KEY;
 
-router.get("/movie/:movie_id", async (req, res) => {
+router.get("/movie/movie_id=:movie_id", async (req, res) => {
   try {
     const { movie_id } = req.params;
     const movieData = await axios.get(`http://www.omdbapi.com/?i=${movie_id}&apikey=${API_KEY}`);
@@ -19,17 +19,21 @@ router.get("/movie/:movie_id", async (req, res) => {
   }
 });
 
-router.get("/search/title/:title", async (req, res) => {
+router.get("/search/title/title=:title&page=:pageNumber", async (req, res) => {
   try {
-    const { title } = req.params;
-    const searchResults = await axios.get(`http://www.omdbapi.com/?s=${title}&apikey=${API_KEY}`);
+    const { title, pageNumber } = req.params;
+    const searchResults = await axios.get(
+      `http://www.omdbapi.com/?s=${title}&page=${pageNumber}&apikey=${API_KEY}`
+    );
     if (searchResults.data.Response === "False") {
       res.status(404).send("We couldn't find any movies.. :(");
     } else {
-      console.log(searchResults.data.Response);
       res.status(200).send(searchResults.data);
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log({ error });
+    res.status(500).send("Could not reach server");
+  }
 });
 
 module.exports = router;
