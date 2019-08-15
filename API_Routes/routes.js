9419473855c2789dedccb5b1bd47d70a7da7822a
Yosefc <yosef.conn@gmail.com>
2019-08-15 10:54:33 +0300

    Reverted

--- API_Routes/routes.js
+++ API_Routes/routes.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const axios = require("axios");
-const resultsController = require("./resultsController");
 
 const router = express.Router();
 
-router.get("/movie/:queryType&:queryString", async (req, res) => {
-  resultsController(req, res);
+const API_KEY = process.env.OMDB_API_KEY;
+
+router.get("/movie/:movie_id", async (req, res) => {
+  try {
+    const { movie_id } = req.params;
+    const movieData = await axios.get(`http://www.omdbapi.com/?i=${movie_id}&apikey=${API_KEY}`);
+    if (movieData.data.Response === "False") {
+      res.status(404).send("Movie not found..");
+    } else {
+      res.status(200).send(movieData.data);
+    }
+  } catch (error) {
+    res.status(500).send("Could not reach server");
+  }
 });
 
-router.get("/search/title/:queryType&:queryString", async (req, res) => {
-  resultsController(req, res);
+router.get("/search/title/:title", async (req, res) => {
+  try {
+    const { title } = req.params;
+    const searchResults = await axios.get(`http://www.omdbapi.com/?s=${title}&apikey=${API_KEY}`);
+    if (searchResults.data.Response === "False") {
+      res.status(404).send("We couldn't find any movies.. :(");
+    } else {
+      console.log(searchResults.data.Response);
+      res.status(200).send(searchResults.data);
+    }
+  } catch (error) {}
 });
 
 module.exports = router;
