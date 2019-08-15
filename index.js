9a5563899b4d11eb156afe31ebb3312ae8286b76
Yosefc <yosef.conn@gmail.com>
2019-08-15 10:42:59 +0300

    Edited index.js

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.get("/Hi", (req, res) => {
-  res.send("hi");
-});
-
 app.use(morgan("dev"));
 app.use("/api", require("./API_Routes/routes"));
 
 const PORT = 4000 || process.env.PORT;
 
 app.listen(PORT, _ => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
