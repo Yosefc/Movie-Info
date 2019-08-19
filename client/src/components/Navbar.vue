5605ac191e125a0e64b4eed0d8be098d1837c671
Yosefc <yosef.conn@gmail.com>
2019-08-19 16:39:40 +0300

    Created Navbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <div class="logo">Movie-Info</div>
+    <nav class="navbar">
+      <router-link class="nav-item" to="/">Home</router-link>
+      <router-link class="nav-item" to="/about">About</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  height: 50px;
+  justify-content: space-between;
+  padding: 0 25px;
+  margin-bottom: 25px;
+  background-color: #292727;
+  color: white;
+  font-size: 1.2rem;
+}
+
+.logo {
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+
+.nav-item {
+  padding: 0 10px;
+  text-decoration: none;
+  color: white;
+}
+</style>
