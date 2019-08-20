e47da1ec7aa2b36f1d60a2e319d77882fc7fca62
Yosefc <yosef.conn@gmail.com>
2019-08-20 11:10:11 +0300

    Added css to Navbar.vue

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,16 +1,32 @@
 <template>
   <header>
     <div class="logo">Movie-Info</div>
+    <div class="form">
+      <input type="text" name id="search-box" />
+      <input type="submit" id="movie-search-btn" value="Go" />
+    </div>
     <nav class="navbar">
-      <router-link class="nav-item" to="/">Home</router-link>
-      <router-link class="nav-item" to="/about">About</router-link>
+      <router-link
+        class="nav-item"
+        v-for="link in navLinks"
+        :key="link.id"
+        :to="link.to"
+      >{{link.name}}</router-link>
     </nav>
   </header>
 </template>
 
 <script>
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  data: () => {
+    return {
+      navLinks: [
+        { id: 1, to: "/", name: "Home" },
+        { id: 2, to: "/about", name: "About" }
+      ]
+    };
+  }
 };
 </script>
 
@@ -18,8 +34,8 @@ export default {
 header {
   display: flex;
   align-items: center;
-  height: 50px;
   justify-content: space-between;
+  height: 65px;
   padding: 0 25px;
   margin-bottom: 25px;
   background-color: #292727;
@@ -36,5 +52,38 @@ header {
   padding: 0 10px;
   text-decoration: none;
   color: white;
+}
+
+.form {
+  display: flex;
+  justify-content: center;
+}
+
+#search-box {
+  border-radius: 30px;
+  padding-right: 50px;
+  padding-left: 8px;
+  height: 25px;
+  border: none;
+  outline: none;
+}
+
+#movie-search-btn {
+  margin-left: -20px;
+  border: none;
+  width: 20px;
+  border-left: 0;
+  border-radius: 0 30px 30px 0;
+  outline: none;
+  background-color: #fffefe;
+  cursor: pointer;
+}
+
+#search-box:focus {
+  box-shadow: 0 0 6px yellow;
+}
+
+#movie-search-btn:focus {
+  box-shadow: 0 0 6px yellow;
 }
 </style>
