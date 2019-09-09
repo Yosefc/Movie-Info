023a2a49b0f756413fe9de0d8caecba20ade7ba8
Yosefc <yosef.conn@gmail.com>
2019-09-09 11:05:20 +0300

    Changed SearchBar to component

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,10 +1,7 @@
 <template>
   <header>
     <div class="logo">Movie-Info</div>
-    <div class="form">
-      <input type="text" name id="search-box" />
-      <input type="submit" id="movie-search-btn" value="Go" />
-    </div>
+    <SearchBar />
     <nav class="navbar">
       <router-link
         class="nav-item"
@@ -22,8 +19,13 @@
 </template>
 
 <script>
+import SearchBar from "@/components/SearchBar";
+
 export default {
   name: "Navbar",
+  components: {
+    SearchBar
+  },
   data: () => {
     return {
       navLinks: [
@@ -63,47 +65,9 @@ header {
   color: white;
 }
 
-.form {
-  display: flex;
-  justify-content: center;
-}
-
-#search-box {
-  border-radius: 30px;
-  padding-right: 50px;
-  padding-left: 8px;
-  height: 25px;
-  border: none;
-  outline: none;
-}
-
-#movie-search-btn {
-  margin-left: -20px;
-  border: none;
-  width: 20px;
-  border-left: 0;
-  border-radius: 0 30px 30px 0;
-  outline: none;
-  background-color: #fffefe;
-  cursor: pointer;
-}
-
-#search-box:focus {
-  box-shadow: 0 0 6px yellow;
-}
-
-#movie-search-btn:focus {
-  box-shadow: 0 0 6px yellow;
-}
-
 @media only screen and (max-width: 600px) {
   .logo {
     font-size: 0.85rem;
-  }
-
-  #search-box {
-    width: 80%;
-    height: 20px;
   }
 
   .mobile-navbar {
---
+++ client/src/components/SearchBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="form">
+    <input type="text" name id="search-box" />
+    <input type="submit" id="movie-search-btn" value="Go" />
+  </div>
+</template>
+
+<style scoped>
+.form {
+  display: flex;
+  justify-content: center;
+}
+
+#search-box {
+  border-radius: 30px;
+  padding-right: 50px;
+  padding-left: 8px;
+  height: 25px;
+  border: none;
+  outline: none;
+}
+
+#movie-search-btn {
+  margin-left: -20px;
+  border: none;
+  width: 20px;
+  border-left: 0;
+  border-radius: 0 30px 30px 0;
+  outline: none;
+  background-color: #fffefe;
+  cursor: pointer;
+}
+
+#search-box:focus {
+  box-shadow: 0 0 6px yellow;
+}
+
+#movie-search-btn:focus {
+  box-shadow: 0 0 6px yellow;
+}
+
+@media only screen and (max-width: 600px) {
+  #search-box {
+    width: 80%;
+    height: 20px;
+  }
+}
+</style>
